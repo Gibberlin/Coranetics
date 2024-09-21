f93e7c8d6c40159e6809157b96bfdd84f4f3b463
Gibberlin <syedeyashin@gmail.com>
2024-09-21 22:35:43 +0530

    fixed Logo

--- app/Logo.tsx
+++ app/Logo.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 
 
-function Logo(props) {
+function Logo() {
     return(
    
       
         <svg 
         xmlns="http://www.w3.org/2000/svg"
-                {...props}
+                
                 viewBox="-15.456 -20.735 1000 100"
                 fill="white"
                 height="200"
